b3bcce40543be45c681b1b9f1e55c6857fa153d1
Tiago <o-seu-email-do-github@exemplo.com>
2026-02-12 14:59:03 +0000

    Update task method sort js advanced

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,46 +5,27 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
-    const defaultComparator = (a, b) => {
-      const aString = String(a);
-      const bString = String(b);
-
-      if (aString < bString) {
-        return -1;
-      }
-
-      if (aString > bString) {
-        return 1;
-      }
-
-      return 0;
-    };
-
-    const comparator = compareFunction || defaultComparator;
-
-    for (let i = 0; i < this.length - 1; i++) {
-      for (let j = 0; j < this.length - 1 - i; j++) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - 1; j++) {
+        let shouldSwap = false;
         const a = this[j];
         const b = this[j + 1];
 
-        if (a === undefined) {
-          if (b !== undefined) {
-            const temp = this[j];
-
-            this[j] = this[j + 1];
-            this[j + 1] = temp;
+        if (compareFunction) {
+          if (compareFunction(a, b) > 0) {
+            shouldSwap = true;
           }
-          continue;
+        } else {
+          if (String(a) > String(b)) {
+            shouldSwap = true;
+          }
         }
 
-        if (b === undefined) {
-          continue;
-        }
-
-        if (comparator(a, b) > 0) {
+        if (shouldSwap) {
           const temp = this[j];
 
           this[j] = this[j + 1];
+
           this[j + 1] = temp;
         }
       }
@@ -52,6 +33,8 @@ function applyCustomSort() {
 
     return this;
   };
+
+  [].__proto__.sort = [].__proto__.sort2;
 }
 
 module.exports = applyCustomSort;
