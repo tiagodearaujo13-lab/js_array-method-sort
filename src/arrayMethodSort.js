0b3888728e7ecce0ea1244a6d92f4018091bd08a
Tiago <o-seu-email-do-github@exemplo.com>
2026-02-12 14:45:32 +0000

    Task done! Array metodo sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,53 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const defaultComparator = (a, b) => {
+      const aString = String(a);
+      const bString = String(b);
+
+      if (aString < bString) {
+        return -1;
+      }
+
+      if (aString > bString) {
+        return 1;
+      }
+
+      return 0;
+    };
+
+    const comparator = compareFunction || defaultComparator;
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        const a = this[j];
+        const b = this[j + 1];
+
+        if (a === undefined) {
+          if (b !== undefined) {
+            const temp = this[j];
+
+            this[j] = this[j + 1];
+            this[j + 1] = temp;
+          }
+          continue;
+        }
+
+        if (b === undefined) {
+          continue;
+        }
+
+        if (comparator(a, b) > 0) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
